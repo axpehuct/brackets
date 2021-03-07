7ff39710381541712fd3f5033988c462d1b18d30
Equento <axpehuct95@gmail.com>
2021-03-07 19:26:50 +0300

    feat: 17/20 test passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+  let array = [];
+  let openBrackets = [];
+  let closedBracket = [];
+  for(let i = 0; i < bracketsConfig.length; ++i){
+    openBrackets.push(bracketsConfig[i][0]);
+    closedBracket.push(bracketsConfig[i][1]);
+  }
+  array.push(str[0]);
+  for(let i = 1; i < str.length; ++i){
+
+    if(closedBracket.includes(str[i])){
+      let bracketIndex = findBracketPair(str[i], bracketsConfig);
+      if(array[array.length -1] === openBrackets[bracketIndex]){
+        array.pop();
+      }
+    }else{
+      array.push(str[i]);
+    }
+  }
+  if(!array.length){
+    return true;
+  }
+  return false;
+}
+function findBracketPair(closeBracket, bracketsConfig){
+  for(let i = 0; i < bracketsConfig.length; ++i){
+    if(bracketsConfig[i][1] == closeBracket){
+      return i;
+    }
+  }
 }
